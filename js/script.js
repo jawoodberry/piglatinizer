60713070c66f3086170a32e75a0d07ad18cfbee4
jawoodberry <jawoodberry@s.sfusd.edu>
2019-10-03 00:31:19 +0000

    First push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 $( "document" ).ready(function() {
 
+function PL(string){
+var vowels = ["a","A","e","E","i","I","o","O","u","O"]
 
+for (i = 0; i < vowels.length; i++) {
+    if(string.charAt(0) == vowels[i].numero)
+        {
+            string = word + "ay";
+            return(word);
+            //return true;
+            console.log("vowel");
+        }
+    else{
+        word = string.slice(1);
+        word = word + "ay";
+        return(word);
+            console.log("not");
+
+    }
+
+
+}
+
+}
+
+$("button").onClick(function(){
+        $(".output").text("");
+        $(".output").text(PL($("input").val()));
+    });
 
 });
