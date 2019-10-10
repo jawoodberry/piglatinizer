234d155d080507cf45e8316c13731c19137bbf75
jawoodberry <jawoodberry@s.sfusd.edu>
2019-10-10 00:10:43 +0000

    Everything, but the seperation of words being translated

--- js/script.js
+++ js/script.js
@@ -1,32 +1,27 @@
-$( "document" ).ready(function() {
-
+$("document").ready(function() {
 function PL(string){
-var vowels = ["a","A","e","E","i","I","o","O","u","O"]
-
-for (i = 0; i < vowels.length; i++) {
-    if(string.charAt(0) == vowels[i].numero)
-        {
-            string = word + "ay";
-            return(word);
-            //return true;
-            console.log("vowel");
-        }
-    else{
-        word = string.slice(1);
-        word = word + "ay";
-        return(word);
-            console.log("not");
-
-    }
-
-
+    // check for vowels
+if (string.charAt(0) === "a" ||string.charAt(0) === "e" ||
+string.charAt(0) === "i" ||string.charAt(0) === "o" ||
+string.charAt(0) === "u"){
+word = string.slice(1);
+first = string.charAt(0);
+word = word + first + "way";
+return(word);
+   // return("");
 }
+else {
+    // stanard
+word = string.slice(1);
+first = string.charAt(0);
+word = word + first + "ay";
+return(word);
+}}
 
-}
+$("button").click(function(){
+    $(".output").text(PL($("input").val()));
 
-$("button").onClick(function(){
-        $(".output").text("");
-        $(".output").text(PL($("input").val()));
-    });
+});
 
+// end
 });
